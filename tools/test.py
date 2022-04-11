5a1c018b67d53bf6feb73b655cd78df4715a2d0c
jscarlson <jcarlson238@gmail.com>
2022-04-11 15:16:10 -0400

    dev

--- tools/test.py
+++ tools/test.py
@@ -11,9 +11,7 @@ from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
                          wrap_fp16_model)
 
-from mmdet.apis import multi_gpu_test
-from mmdet.apis.test import single_gpu_test
-print(inspect.getsource(single_gpu_test))
+from mmdet.apis import multi_gpu_test, single_gpu_test
 from mmdet.datasets import (build_dataloader, build_dataset,
                             replace_ImageToTensor)
 from mmdet.models import build_detector
