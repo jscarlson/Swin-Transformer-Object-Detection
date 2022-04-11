1e7580ef237ce3deee743a48cf35b66e57886b42
jscarlson <jcarlson238@gmail.com>
2022-04-11 15:04:32 -0400

    dev

--- tools/test.py
+++ tools/test.py
@@ -192,6 +192,7 @@ def main():
 
     if not distributed:
         model = MMDataParallel(model, device_ids=[0])
+        print(args.show_options)
         outputs = single_gpu_test(model, data_loader, args.show, args.show_dir,
                                   args.show_score_thr, **args.show_options)
     else:
