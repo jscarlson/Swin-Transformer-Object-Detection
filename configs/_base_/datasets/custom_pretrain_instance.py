f3a9df09f322261dde437dbcc4ab9269bcba85b5
jscarlson <jcarlson238@gmail.com>
2022-04-11 18:14:01 -0400

    dev

--- configs/_base_/datasets/custom_pretrain_instance.py
+++ configs/_base_/datasets/custom_pretrain_instance.py
@@ -3,6 +3,7 @@ _base_ = './coco_instance.py'
 
 # 1. dataset settings
 dataset_type = 'CocoDataset'
+data_root = '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_more/'
 classes = ('character', 'word',)
 data = dict(
     samples_per_gpu=1,
@@ -11,18 +12,18 @@ data = dict(
         type=dataset_type,
         # explicitly add your class names to the field `classes`
         classes=classes,
-        ann_file='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/train80.json',
-        img_prefix='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/images'),
+        ann_file=data_root+'train90.json',
+        img_prefix=data_root+'images/'),
     val=dict(
         type=dataset_type,
         # explicitly add your class names to the field `classes`
         classes=classes,
-        ann_file='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/test20.json',
-        img_prefix='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/images'),
+        ann_file=data_root+'test10.json',
+        img_prefix=data_root+'images/'),
     test=dict(
         type=dataset_type,
         # explicitly add your class names to the field `classes`
         classes=classes,
-        ann_file='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/test20.json',
-        img_prefix='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/images'))
+        ann_file=data_root+'test10.json',
+        img_prefix=data_root+'images/'))
 evaluation = dict(metric=['bbox', 'segm'], save_best='bbox_mAP')
