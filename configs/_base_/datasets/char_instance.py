aa4b45030c8be7fb2c0fea01ef6136f7ba69ab10
jscarlson <jcarlson238@gmail.com>
2022-04-11 14:08:15 -0400

    dev

--- configs/_base_/datasets/char_instance.py
+++ configs/_base_/datasets/char_instance.py
@@ -25,25 +25,3 @@ data = dict(
         classes=classes,
         ann_file='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/test20.json',
         img_prefix='/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_simpler_fewer/images'))
-
-# 2. model settings
-
-# explicitly over-write all the `num_classes` field from default 80 to 5.
-model = dict(
-    roi_head=dict(
-        bbox_head=[
-            dict(
-                type='Shared2FCBBoxHead',
-                # explicitly over-write all the `num_classes` field from default 80 to 5.
-                num_classes=2),
-            dict(
-                type='Shared2FCBBoxHead',
-                # explicitly over-write all the `num_classes` field from default 80 to 5.
-                num_classes=2),
-            dict(
-                type='Shared2FCBBoxHead',
-                # explicitly over-write all the `num_classes` field from default 80 to 5.
-                num_classes=2)],
-    # explicitly over-write all the `num_classes` field from default 80 to 5.
-    mask_head=dict(num_classes=2)))
-
