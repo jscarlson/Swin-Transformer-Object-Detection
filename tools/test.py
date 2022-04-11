f6825db723674d98a632d1ea180ba7d07abadb44
jscarlson <jcarlson238@gmail.com>
2022-04-11 15:12:49 -0400

    dev

--- tools/test.py
+++ tools/test.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import warnings
+import inspect
 
 import mmcv
 import torch
@@ -11,7 +12,7 @@ from mmcv.runner import (get_dist_info, init_dist, load_checkpoint,
                          wrap_fp16_model)
 
 from mmdet.apis import multi_gpu_test, single_gpu_test
-print(single_gpu_test)
+print(inspect.getsource(single_gpu_test))
 from mmdet.datasets import (build_dataloader, build_dataset,
                             replace_ImageToTensor)
 from mmdet.models import build_detector
