a8874258db9e5ebfa632b9945b651fc52cbdfb50
jscarlson <jcarlson238@gmail.com>
2022-04-11 13:47:41 -0400

    dev

--- configs/_base_/datasets/char_instance.py
+++ configs/_base_/datasets/char_instance.py
@@ -26,8 +26,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=1,
+    workers_per_gpu=1,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'train80.json',
