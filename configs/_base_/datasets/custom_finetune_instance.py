b566583d53388f7e0545d38873c54de2291a4be4
jscarlson <jcarlson238@gmail.com>
2022-04-11 15:35:18 -0400

    dev

---
+++ configs/_base_/datasets/custom_finetune_instance.py
@@ -0,0 +1,28 @@
+# the new config inherits the base configs to highlight the necessary modification
+_base_ = './coco_instance.py'
+
+# 1. dataset settings
+dataset_type = 'CocoDataset'
+data_root = "/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/ocr_datasets/newspaper/"
+classes = ('character', 'word',)
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file=data_root+'train70-burlington-times-news-1989.json',
+        img_prefix=data_root+'lines/'),
+    val=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file=data_root+'test30-burlington-times-news-1989.json',
+        img_prefix=data_root+'lines/'),
+    test=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file=data_root+'test30-burlington-times-news-1989.json',
+        img_prefix=data_root+'lines/'))
