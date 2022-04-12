2b7b2cade68906b91a2ebd28fb84dbb80f81826d
jscarlson <jcarlson238@gmail.com>
2022-04-11 21:02:01 -0400

    dev

--- configs/swin/single_class_pretrain_cascade_swin_small.py
+++ configs/swin/single_class_pretrain_cascade_swin_small.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/cascade_mask_rcnn_swin_fpn.py',
-    '../_base_/datasets/custom_pretrain_instance.py',
+    '../_base_/datasets/single_class_custom_pretrain_instance.py',
     '../_base_/schedules/schedule_1x.py', 
     '../_base_/default_runtime.py'
 ]
