b350aa1a0d196b2921b742dbf6e05afc53704d5d
jscarlson <jcarlson238@gmail.com>
2022-07-13 10:06:35 -0400

    dev

--- configs/_base_/datasets/custom_finetune_instance.py
+++ configs/_base_/datasets/custom_finetune_instance.py
@@ -17,13 +17,13 @@ data = dict(
     val=dict(
         type=dataset_type,
         classes=classes,
-        ann_file=data_root + 'noisy_test30sofar_highres_expanded_comma_corrected.json',
+        ann_file=data_root + 'noisy_val15sofar_highres_expanded_comma_corrected.json',
         img_prefix=data_root + 'noisy_lines2/',
     ),
     test=dict(
         type=dataset_type,
         classes=classes,
-        ann_file=data_root + 'noisy_test30sofar_highres_expanded_comma_corrected.json',
+        ann_file=data_root + 'noisy_test15sofar_highres_expanded_comma_corrected.json',
         img_prefix=data_root + 'noisy_lines2/',
     ))
 evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP', classwise=True)
