1d22a79bb29c63f342b4ca1095a623db71a5329f
jscarlson <jcarlson238@gmail.com>
2022-07-20 10:56:07 -0700

    dev

---
+++ configs/_base_/datasets/custom_pretrain_instance_japanese_vertical.py
@@ -0,0 +1,26 @@
+# the new config inherits the base configs to highlight the necessary modification
+_base_ = './coco_instance.py'
+
+# 1. dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/synthtxt_jp_vertical/'
+classes = ('character',)
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root+'train80.json',
+        img_prefix=data_root+'images/'),
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root+'val10.json',
+        img_prefix=data_root+'images/'),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root+'test10.json',
+        img_prefix=data_root+'images/'))
+evaluation = dict(metric='bbox', save_best='bbox_mAP', classwise=True, interval=1)
