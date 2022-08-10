bb18087cdc10062c56ea185ea6dadb7051abb8f9
jscarlson <jcarlson238@gmail.com>
2022-08-10 18:29:36 -0400

    dev

---
+++ configs/_base_/datasets/custom_finetune_instance_ww.py
@@ -0,0 +1,29 @@
+# the new config inherits the base configs to highlight the necessary modification
+_base_ = './coco_instance.py'
+
+# 1. dataset settings
+dataset_type = 'CocoDataset'
+data_root = "/mnt/122a7683-fa4b-45dd-9f13-b18cc4f4a187/ocr_datasets/ww/"
+classes = ('char',)
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train70.json',
+        img_prefix=data_root + 'images/',
+    ),
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'val15.json',
+        img_prefix=data_root + 'images/',
+    ),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test15.json',
+        img_prefix=data_root + 'images/',
+    ))
+evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP', classwise=True)
