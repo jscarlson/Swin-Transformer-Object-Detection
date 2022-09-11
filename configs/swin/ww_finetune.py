5a6ff26c60752ac5b5792e57623d1024bb065003
jscarlson <jcarlson238@gmail.com>
2022-09-10 23:15:48 -0400

    dev

--- configs/swin/ww_finetune.py
+++ configs/swin/ww_finetune.py
@@ -128,7 +128,7 @@ optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), wei
 lr_config = dict(step=[27, 33])
 runner = dict(type='EpochBasedRunner', max_epochs=36)
 
-checkpoint_config = dict(interval=1, max_keep_ckpts=10)
+checkpoint_config = dict(interval=1)
 
 # do not use mmdet version fp16
 """fp16 = None
